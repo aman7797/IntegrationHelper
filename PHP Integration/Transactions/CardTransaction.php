ccdf50e8eed256ef151baf083db409a3c9668668
Aman Lalpuria <aman.lalpuria@juspay.in>
2019-05-15 21:00:56 +0530

    Card Transaction

---
+++ PHP Integration/Transactions/CardTransaction.php
@@ -0,0 +1,48 @@
+<?php
+
+$curl = curl_init();
+$order_id="ORD_1557929645";
+$merchant_id="aman_prod";
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.juspay.in/txns",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"order_id\"\r\n\r\n$order_id\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"merchant_id\"\r\n\r\n$merchant_id\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"payment_method_type\"\r\n\r\nCARD\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"payment_method\"\r\n\r\nMASTERCARD\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"card_number\"\r\n\r\n5419190202700020\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"card_exp_year\"\r\n\r\n22\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"card_exp_month\"\r\n\r\n07\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"name_on_card\"\r\n\r\ntest\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"card_security_code\"\r\n\r\n015\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"save_to_locker\"\r\n\r\ntrue\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"redirect_after_payment\"\r\n\r\ntrue\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"format\"\r\n\r\njson\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--",
+  CURLOPT_HTTPHEADER => array(
+    "Accept: */*",
+    "Authorization: Basic NDQ0RTBGQjVERjk0NjMwOEIzRDY0ODgxRkRCQ0M3Og==",
+    "Cache-Control: no-cache",
+    "Connection: keep-alive",
+    "Host: api.juspay.in",
+    "Postman-Token: 733cf7da-910f-4505-9ebb-412ca09508a5,4945d6cb-bfa6-41db-bd48-81bc861f9fc2",
+    "User-Agent: PostmanRuntime/7.11.0",
+    "accept-encoding: gzip, deflate",
+    "cache-control: no-cache",
+    "content-length: 1482",
+    "content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW"
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
+
+// Response 
+// { "order_id": "ORD_1557929645",
+//    "txn_id": "aman_prod-ORD_1557929645-2",
+//    "txn_uuid": "bar4whobdudkiopg",
+//    "status": "PENDING_VBV",
+//    "payment": { "authentication": { "method": "GET",
+//    "url": "https://api.juspay.in/pay/start/aman_prod/bar4whobdudkiopg" } } }
